dcb531e1e56f8f96aa1e43590648383e96eaf99e
Martijn <oldmartijntje@gmail.com>
2024-10-03 17:53:19 +0200

    selecting username

---
+++ docs/mqtt-receiver/windows-chat-app/script.js
@@ -0,0 +1,51 @@
+
+const overlay = document.querySelector('.overlay');
+const usernameInput = document.getElementById('username');
+const okButton = document.querySelector('.dialog-content button');
+const closeButton = document.querySelector('.dialog-title button');
+const usernameEditButton = document.querySelector('.info-area button');
+
+const localSettings = localStorage.getItem('mqtt-windows95-settings');
+let settings = localSettings ? JSON.parse(localSettings) : {
+    username: '',
+};
+let enteredUsername = false;
+
+if (settings.username && settings.username.trim() !== '') {
+    overlay.style.display = 'none';
+    enteredUsername = true;
+    setUsernameLabel()
+}
+
+usernameEditButton.addEventListener('click', () => {
+    overlay.style.display = 'flex';
+    usernameInput.value = settings.username;
+});
+
+closeButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    if (!enteredUsername) {
+        setTimeout(() => {
+            overlay.style.display = 'flex';
+        }, 100);
+    }
+});
+
+okButton.addEventListener('click', () => {
+    const username = usernameInput.value.trim();
+    if (username) {
+        settings.username = username.split(' ').join('_');
+        console.log(`Username set to: ${username}`);
+        overlay.style.display = 'none';
+        localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
+        enteredUsername = true;
+        setUsernameLabel();
+    } else {
+        alert('Please enter a username');
+    }
+});
+
+function setUsernameLabel() {
+    const usernameLabel = document.getElementById('username-field');
+    usernameLabel.textContent = settings.username;
+}
