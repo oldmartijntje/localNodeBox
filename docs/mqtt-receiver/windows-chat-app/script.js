dabc161ec42cd94ecc66e6815368235f78456cff
Martijn <oldmartijntje@gmail.com>
2024-10-04 16:50:02 +0200

    messageing

--- docs/mqtt-receiver/windows-chat-app/script.js
+++ docs/mqtt-receiver/windows-chat-app/script.js
@@ -4,6 +4,8 @@ const okButton = document.querySelector('.dialog-content button');
 const closeButton = document.querySelector('.dialog-title button');
 const usernameEditButton = document.querySelector('.info-area button');
 const contactsArea = document.querySelector('.contacts-area');
+const sendMessageButton = document.querySelector('.input-area button');
+const messageInput = document.querySelector('.input-area input');
 const defaultSettings = {
     username: '',
     userId: generateUUID(),
@@ -54,6 +56,7 @@ let testPing = {
     userId: 'uuid',
     time: new Date().toLocaleTimeString(),
 }
+
 const client = mqtt.connect('wss://test.mosquitto.org:8081');
 client.on('connect', () => {
     client.subscribe(`${settings.frequencyUsed}/${settings.userId}`, (err) => {
@@ -117,6 +120,42 @@ okButton.addEventListener('click', () => {
     }
 });
 
+sendMessageButton.addEventListener('click', () => {
+    const message = replaceAllNoneAlphaNumeric(messageInput.value.trim(), true);
+    if (message) {
+        messageInput.value = '';
+        const messageDesign = JSON.parse(JSON.stringify(testMessage));
+        messageDesign.message = message;
+        messageDesign.username = settings.username;
+        messageDesign.time = `${new Date()}`;
+        const allChgats = { ...settings.activeContacts, ...settings.chatRooms };
+        messageDesign.userId = settings.userId;
+        if (allChgats[activeChatId].chatroom) {
+            messageDesign.channelId = activeChatId;
+            for (const [key, value] of Object.entries(settings.activeContacts)) {
+                if (key === settings.userId) {
+                    continue;
+                }
+                console.log(`${settings.frequencyUsed}/${key}`, JSON.stringify(messageDesign))
+                client.publish(`${settings.frequencyUsed}/${key}`, JSON.stringify(messageDesign));
+            }
+        } else {
+            messageDesign.channelId = settings.userId;
+            messageDesign.receiverId = activeChatId;
+            client.publish(`${settings.frequencyUsed}/${activeChatId}`, JSON.stringify(messageDesign));
+        }
+        if (!settings.chatHistory[activeChatId]) {
+            settings.chatHistory[activeChatId] = [];
+        }
+        settings.chatHistory[activeChatId].push({ username: settings.username, message, time: messageDesign.time, userId: settings.userId });
+        localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
+        selectChat(activeChatId);
+        messageInput.value = '';
+    }
+});
+
+
+
 setInterval(() => {
     sendPing();
 }, 10000); // 10 seconds
@@ -128,15 +167,18 @@ setInterval(() => {
 function ckeckChatStatus() {
     let changed = false;
     for (const [key, value] of Object.entries(settings.activeContacts)) {
+        let changedThisId = false;
         if (value.time + 60000 < Date.now()) {
             // 1 minute inactivity
             if (value.status == 2) {
                 value.status = 1;
                 changed = true;
+                changedThisId = true;
             } else if (value.status < 2 && value.time + 300000 < Date.now()) {
                 // 5 ,minutes inactivity
                 value.status = 0;
                 changed = true;
+                changedThisId = true;
                 if ((!settings.chatHistory[value.identifier] || settings.chatHistory[value.identifier].length === 0) && value.time + 600000 < Date.now()) {
                     // 10 minutes of inactivity
                     settings.chatHistory[value.identifier] = [];
@@ -148,6 +190,9 @@ function ckeckChatStatus() {
                 }
             }
         }
+        if (activeChatId === key && changedThisId) {
+            selectChat(key);
+        }
     }
     localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
     if (changed) {
@@ -155,7 +200,11 @@ function ckeckChatStatus() {
     }
 }
 
-function replaceAllNoneAlphaNumeric(str) {
+function replaceAllNoneAlphaNumeric(str, allowBraces = false) {
+    str = str.toLowerCase();
+    if (allowBraces) {
+        return str.replace(/[^a-z0-9_\-\[\]\(\):\/\.\?&= ]/g, '¿');
+    }
     return str.replace(/[^a-z0-9_-]/g, '¿');
 }
 
@@ -225,16 +274,20 @@ function getLastActiveTime(identifier) {
 function selectChat(chatId) {
     activeChatId = chatId;
     const chat = { ...settings.activeContacts, ...settings.chatRooms }[chatId];
+    if (chat.unReadMessages > 0) { 
+        chat.unReadMessages = 0;
+        localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
+        renderContacts();
+    }
     const chatArea = document.querySelector('.chat-area');
     chatArea.innerHTML = '';
-    console.log(chat)
     const activeChatName = document.getElementById('active-chat-name');
     activeChatName.textContent = `${chat.username} ${!chat.chatroom ? ` - ${getLastActiveTime(chatId)}` : ''}`;
     if (settings.chatHistory[chat.identifier]) {
         settings.chatHistory[chat.identifier].forEach(message => {
             const messageElement = document.createElement('div');
             messageElement.classList.add('message');
-            messageElement.innerHTML = `<div class="message">${message.username}: ${message.message} <span class="time">${message.time}</span></div>`;
+            messageElement.innerHTML = `<div class="message"><span class="time">${new Date(message.time).toLocaleTimeString()}</span> ${message.username}: ${message.message}</div>`;
             chatArea.appendChild(messageElement);
         });
     }
@@ -254,6 +307,8 @@ function pingedByUser(messageJson) {
             }
             if (!settings.activeContacts[key]) {
                 settings.activeContacts[key] = value;
+            } else if (value.time < settings.activeContacts[key].time) {
+                continue;
             } else {
                 settings.activeContacts[key].time = value.time;
                 settings.activeContacts[key].status = value.status;
@@ -264,7 +319,7 @@ function pingedByUser(messageJson) {
     if (settings.activeContacts.length === 0) {
         settings.activeContacts[messageJson.userId] = {
             username: messageJson.username,
-            time: Date.now(),
+            time: messageJson.time,
             status: 2,
             unReadMessages: 0,
             identifier: messageJson.userId,
@@ -274,18 +329,26 @@ function pingedByUser(messageJson) {
         if (!settings.activeContacts[messageJson.userId]) {
             settings.activeContacts[messageJson.userId] = {
                 username: messageJson.username,
-                time: Date.now(),
+                time: messageJson.time,
                 status: 2,
                 unReadMessages: 0,
                 identifier: messageJson.userId,
                 chatroom: false,
             };
         } else {
-            settings.activeContacts[messageJson.userId].time = Date.now();
-            settings.activeContacts[messageJson.userId].status = 2;
+            settings.activeContacts[messageJson.userId].time = messageJson.time;
+            const previousStatus = settings.activeContacts[messageJson.userId].status;
+            const previousUsername = settings.activeContacts[messageJson.userId].username;
             settings.activeContacts[messageJson.userId].username = replaceAllNoneAlphaNumeric(messageJson.username);
+            settings.activeContacts[messageJson.userId].status = 2;
+            if (previousUsername !== messageJson.username && activeChatId === messageJson.userId) {
+                selectChat(messageJson.userId);
+            } else if (previousStatus !== 2 && activeChatId === messageJson.userId) {
+                selectChat(messageJson.userId);
+            }
         }
     }
+
     localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
     renderContacts()
 }
@@ -299,7 +362,24 @@ function receiveMessage(topic, message) {
         return;
     }
     if (topic === `${settings.frequencyUsed}/${settings.userId}`) {
-        console.log(`Received message: ${msg} from topic: ${topic}`);
+        if (msg.userId === settings.userId || !msg.userId || settings.blockedUuids.includes(msg.userId)) {
+            return;
+        }
+        if (settings.activeContacts[msg.channelId] && activeChatId !== msg.channelId) {
+            settings.activeContacts[msg.channelId].unReadMessages = settings.activeContacts[msg.channelId].unReadMessages ? settings.activeContacts[msg.channelId].unReadMessages + 1 : 1;
+        } else if (settings.chatRooms[msg.channelId] && activeChatId !== msg.channelId) {
+            settings.chatRooms[msg.channelId].unReadMessages = settings.chatRooms[msg.channelId].unReadMessages ? settings.chatRooms[msg.channelId].unReadMessages + 1 : 1;
+        }
+        if (!settings.chatHistory[msg.channelId]) {
+            settings.chatHistory[msg.channelId] = [];
+        }
+        settings.chatHistory[msg.channelId].push({ username: msg.username, message: msg.message, time: msg.time, userId: msg.userId });
+        console.log(msg, settings.chatHistory[msg.channelId])
+        localStorage.setItem('mqtt-windows95-settings', JSON.stringify(settings));
+        if (activeChatId === msg.channelId) {
+            selectChat(msg.userId);
+        }
+        renderContacts();
     } else if (topic === pingURL) {
         pingedByUser(msg);
     } else if (topic === pingRequestURL) {
@@ -317,7 +397,7 @@ function sendPing(showContacts = false) {
     if (showContacts) {
         pingDesign.allContacts = settings.activeContacts;
     }
-    pingDesign.time = new Date().toLocaleTimeString();
+    pingDesign.time = `${new Date()}`;
     client.publish(pingURL, JSON.stringify(pingDesign));
 }
 
